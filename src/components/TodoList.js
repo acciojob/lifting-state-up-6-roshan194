05769bd3a3bb551f00e4c7e2df1a4bdca6e38d1c
Roshan Kumar <roshankumarrk9570@gmail.com>
2025-06-09 17:40:14 +0530

    fix

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,7 @@ function TodoList({ todos, onComplete }) {
   return (
     <ul>
       {todos.map((todo) => (
-        <li key={todo.id} style={{ textDecoration: todo.completed }}>
+        <li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
           {todo.text}
           {!todo.completed && (
             <button onClick={() => onComplete(todo.id)}>
